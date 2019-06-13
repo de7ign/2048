b5d4e3f85c7449b2ef2d5f9b77a51b6a696bc57c
Nihal Murmu <nhlmrm@gmail.com>
2019-06-13 18:14:45 +0530

    Add basic layout
    defined basic layout and installed 'react-native-vector-icons'

--- App.js
+++ App.js
@@ -6,24 +6,80 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 type Props = {};
 export default class App extends Component<Props> {
+  handleRestartButtonPress() {
+    alert("restart button clicked");
+  }
   render() {
+    const { width } = Dimensions.get("window");
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View
+          style={{
+            width: width * 0.85,
+            height: width * 0.16,
+            marginBottom: 10,
+            flexDirection: "row",
+            justifyContent: "flex-end"
+          }}
+        >
+          <Icon.Button
+            name="replay"
+            style={{ flex: 1 }}
+            onPress={this.handleRestartButtonPress}
+          >
+            <Text
+              style={{
+                fontFamily: "Arial",
+                fontSize: 15,
+                color: "white",
+                marginEnd: 10
+              }}
+            >
+              Restart
+            </Text>
+          </Icon.Button>
+        </View>
+        <View
+          style={{
+            width: width * 0.85,
+            height: width * 0.2,
+            marginBottom: 10,
+            flexDirection: "row"
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: "gray",
+              borderRadius: 5,
+              marginEnd: 5
+            }}
+          >
+            <Text>Score</Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: "gray",
+              borderRadius: 5,
+              marginStart: 5
+            }}
+          ><Text>Highest Score</Text></View>
+        </View>
+        <View
+          style={{
+            backgroundColor: "gray",
+            borderRadius: 5,
+            width: width * 0.85,
+            height: width * 0.85
+          }}
+        ><Text>Board</Text></View>
       </View>
     );
   }
@@ -32,18 +88,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightblue"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
