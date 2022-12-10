178366215050f2e2a0bb0855f50e16db94326701
Nihal Murmu <31739586+nihalmurmu@users.noreply.github.com>
2022-12-10 23:54:32 +0530

    Add basic cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { GridContainerComponent } from './grid-container/grid-container.component';
+import { GridCardComponent } from './grid-card/grid-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GridContainerComponent
+    GridContainerComponent,
+    GridCardComponent
   ],
   imports: [
     BrowserModule
